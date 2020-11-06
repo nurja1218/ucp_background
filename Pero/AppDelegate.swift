e730c3f2fbd669d4739839cf7557d0ae6bfc5133
junsungpark <jimmy@junsoft.org>
2020-11-06 14:45:20 +0900

    메뉴 사항 적용

--- Pero/AppDelegate.swift
+++ Pero/AppDelegate.swift
@@ -38,13 +38,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+/*
+     백그라운드 처리는 PC 카카오톡과 같은 형태로 UI창을 닫더라도 Application은 종료되지 않고 계속 실행되어 있는 형태로 요청드립니다.
+     백그라운드(UI는 닫혔으나 Application종료되지 않은 상태)에서의 기능은
+     1. 연결정보 연결알림
+     2. 로그인 정보 및 로그 오프
+     3. 버전정보
+     4. 종료
+     5. PERO(제품) 정보 및 업데이트 알림
+     */
+    
+    
+    
     func constructMenu() {
       let menu = NSMenu()
 
-      menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
-      menu.addItem(NSMenuItem.separator())
-      menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+      
+        menu.addItem(NSMenuItem(title: "연결정보 연결알림", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: ""))
+      
+    //    menu.addItem(NSMenuItem.separator())
+     
+  //      menu.addItem(NSMenuItem(title: "로그인 정보 및 로그 오프", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+      
+    
+   //     menu.addItem(NSMenuItem.separator())
+        
+        menu.addItem(NSMenuItem(title: "버전정보", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+
+ //       menu.addItem(NSMenuItem.separator())
+        
+        menu.addItem(NSMenuItem(title: "PERO(제품) 정보 및 업데이트 알림", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+
+        menu.addItem(NSMenuItem.separator())
+        
+        menu.addItem(NSMenuItem(title: "종료", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
 
       statusItem.menu = menu
     }
